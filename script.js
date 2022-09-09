9468e21d52b3735ff2390625d045f582be8b9734
MaryBayt <94868308+MaryBayt@users.noreply.github.com>
2022-09-10 00:00:52 +0300

    minor fix

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ const allHands = document.querySelectorAll('.all-hands');
 function setDate () {
     const now = new Date();
     const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    const secondsDegrees = seconds / 60 * 360 + 90;
     const minutes = now.getMinutes();
-    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    const minutesDegrees = minutes / 60 * 360 + 90;
     const hours = now.getHours();
-    const hoursDegrees = ((hours / 60) * 360) + 90;
+    const hoursDegrees = hours % 12 / 12 * 360 + 90;
     secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
     minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
     hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
